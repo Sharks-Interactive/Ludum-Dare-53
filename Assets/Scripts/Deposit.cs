2e6f2c3c2af5f3ac843be889a86b06b443c94534
Shark Desktop (Owen) <owensilva1282@gmail.com>
2023-05-01 00:39:35 -0400

    Ugh

--- Assets/Scripts/Deposit.cs
+++ Assets/Scripts/Deposit.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Deposit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SItem HeldItem
     {
-        
+        get => _heldItem;
+        set
+        {
+            _heldItem = value;
+            foreach (GameObject item in Item)
+            {
+                item.SetActive(false);
+            }
+            if (_heldItem == null) return;
+            Item[_heldItem.Type - 1].SetActive(true);
+        }
     }
+    private SItem _heldItem;
 
-    // Update is called once per frame
-    void Update()
+    public GameObject[] Item;
+
+    private void OnTriggerStay(Collider other)
     {
-        
+        Player _player = other.GetComponent<Player>();
+        if (_player != null)
+        {
+            if (Input.GetActionInput(_player.ID).isPressed)
+            {
+                if (_player.HeldItem != null && HeldItem == null)
+                {
+                    HeldItem = _player.HeldItem;
+                    _player.HeldItem = null;
+
+                    Input.Feedback(_player.ID, new Vector2(1, 0), 250);
+                    return;
+                }
+                if (_player.HeldItem == null && HeldItem != null)
+                {
+                    _player.HeldItem = HeldItem;
+                    HeldItem = null;
+
+                    Input.Feedback(_player.ID, new Vector2(1, 0), 250);
+                    return;
+                }
+            }
+        }
     }
 }
